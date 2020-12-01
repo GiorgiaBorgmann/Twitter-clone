c448ecddc1869acbf6d1e647516b029159b6a88d
Giorgia Borgmann <66149711+GiorgiaBorgmann@users.noreply.github.com>
2020-12-01 18:34:18 +0200

    Milestone_2

--- project-02/src/App.js
+++ project-02/src/App.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Form from './components/Form.js'
 import Tweet from './components/Tweet.js'
+
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [tweetList, setTweets] = useState([]);
 
-  useEffect(() => {
-    const tweetList = JSON.parse(localStorage.getItem("tweetList"));
-    if (tweetList) {
-      setTweets(tweetList);
-    }
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("tweetList", JSON.stringify(tweetList));
-  }, [tweetList]);
-
-
   return (
     <div >
       <Form inputText={inputText} tweetList={tweetList} setTweets={setTweets} setInputText={setInputText} />
-      {tweetList.map((tweet) => (
-        < Tweet key={tweet.id} tweet={tweet} />))}
+      {tweetList.map((tweet, index) => (
+        < Tweet key={index} tweet={tweet} />))}
     </div>
   );
 }
 
-export default App;
+export default App
--- project-02/src/components/Form.js
+++ project-02/src/components/Form.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 function Form({ setInputText, setTweets, tweetList, inputText }) {
     const [tweetTooLong, setTweetTooLong] = useState(false)
     const handleTweetToLong = (event) => {
@@ -10,16 +11,75 @@ function Form({ setInputText, setTweets, tweetList, inputText }) {
             setInputText(event.target.value)
         }
     }
+
+    const URL = "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet"
+
+    const dataRecieve = async (newTweets) => {
+        const tweetList = await fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(newTweets),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+        if (!tweetList.ok) Error("error");
+        const data = await tweetList.json();
+        console.log(data)
+    }
+    useEffect(() => {
+        const getTweets = async () => {
+            showLoader()
+            const tweetList = await fetch(URL);
+            const data = await tweetList.json();
+            setTweets(data.tweets);
+            console.log(data)
+            if (data) {
+                hideLoader()
+            }
+        };
+        getTweets();
+    }, []);
+
+    const addTweet = (text) => {
+        const newTweets = {}
+        newTweets.content = text;
+        newTweets.date = new Date().toISOString()
+        newTweets.userName = 'Giorgia'
+        dataRecieve(newTweets)
+        showLoader()
+        setTimeout(() => {
+            setTweets([newTweets, ...tweetList])
+            hideLoader()
+        }, 2000)
+    }
     const submitTweetHandler = (event) => {
         event.preventDefault()
-        setTweets([{ text: inputText, user: 'Giorgia', date: new Date(), id: Math.random() }, ...tweetList])
+        addTweet(inputText)
         setInputText('')
     }
+    const PageLoader = () => {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    const [loading, setLoading] = useState(false)
+    const usePageLoader = () => {
+        return [
+            loading ? PageLoader() : null,
+            () => setLoading(true),
+            () => setLoading(false)]
+
+    }
+    const [loader, showLoader, hideLoader] = usePageLoader()
+
     return (
         <div>
-            <textarea value={inputText} onChange={e => handleTweetToLong(e)} placeholder="What you have in mind..."></textarea>
-            <button disabled={tweetTooLong} onClick={submitTweetHandler}>Tweet</button>
+            <textarea value={inputText} onChange={event => handleTweetToLong(event)} placeholder="What you have in mind..."></textarea>
+            {!loading ? <button disabled={tweetTooLong} onClick={submitTweetHandler}>Tweet</button> : null}
+            { loader}
         </div>
+
     )
 }
 export default Form
